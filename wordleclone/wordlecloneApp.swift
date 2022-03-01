7a1c85725bba51e2382761765e69479c8ce486b5
Shameem Reza <codechef@yahoo.com>
2022-03-01 11:40:55 +0600

    Initial Setup :fire:

---
+++ wordleclone/wordlecloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordlecloneApp.swift
+//  wordleclone
+//
+//  Created by Shameem Reza on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct wordlecloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
